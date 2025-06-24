c8b7982ef98f617300f17d5183b9f869379caca3
jim price <shadeops@gmail.com>
2025-06-23 21:16:25 -0700

    Refactor json parsing into its own struct

--- src/main.zig
+++ src/main.zig
@@ -4,16 +4,16 @@ const pdapi = @import("playdate_api_definitions.zig");
 const panic_handler = @import("panic_handler.zig");
 
 const level = @embedFile("library.json");
+const debug = builtin.mode == .Debug;
 
 pub const panic = panic_handler.panic;
 
 const GlobalState = struct {
     playdate: *pdapi.PlaydateAPI,
-    tree: *pdapi.LCDBitmap,
     font: *pdapi.LCDFont,
     pos: Position,
     tile_map: *TiledBG,
-    bitlib: BitmapLibrary,
+    bitlib: BitmapLib,
 };
 
 const Position = struct {
@@ -21,19 +21,18 @@ const Position = struct {
     y: i8,
 };
 
-const debug = builtin.mode == .Debug;
-
-const BitmapLibrary = struct {
+const BitmapLib = struct {
     const max_bitmaps = 128;
 
     bitmaps: []*pdapi.LCDBitmap,
     playdate: *const pdapi.PlaydateAPI,
-    tmp_res: [2]c_int = [2]c_int{ 0, 0 },
-    tmp_is_res: bool = false,
 
-    fn init(playdate: *const pdapi.PlaydateAPI) BitmapLibrary {
-        const bitmaps_ptr: [*]*pdapi.LCDBitmap = @ptrCast(@alignCast(playdate.system.realloc(null, @sizeOf(*pdapi.LCDBitmap) * max_bitmaps,) orelse unreachable));
-        var bitlib = BitmapLibrary{
+    fn init(playdate: *const pdapi.PlaydateAPI) BitmapLib {
+        const bitmaps_ptr: [*]*pdapi.LCDBitmap = @ptrCast(@alignCast(playdate.system.realloc(
+            null,
+            @sizeOf(*pdapi.LCDBitmap) * max_bitmaps,
+        ) orelse unreachable));
+        var bitlib = BitmapLib{
             .bitmaps = bitmaps_ptr[0..max_bitmaps],
             .playdate = playdate,
         };
@@ -42,38 +41,58 @@ const BitmapLibrary = struct {
         return bitlib;
     }
 
-    fn addMap(self: *BitmapLibrary, bitmap: *pdapi.LCDBitmap) error{LibraryFull}!void {
+    fn deinit(self: *BitmapLib) void {
+        for (self.bitmaps) |bitmap| {
+            self.playdate.graphics.freeBitmap(bitmap);
+        }
+        self.bitmaps.len = 0;
+    }
+
+    fn addMap(self: *BitmapLib, bitmap: *pdapi.LCDBitmap) error{LibraryFull}!void {
         if (self.bitmaps.len + 1 >= max_bitmaps) return error.LibraryFull;
         self.bitmaps.len += 1;
         self.bitmaps[self.bitmaps.len - 1] = bitmap;
     }
+};
+
+const BitmapLibParser = struct {
+    in_spec: bool = false,
+    resx: c_int = 0,
+    resy: c_int = 0,
+    has_mask: bool = false,
+    bitlib: *BitmapLib,
 
     fn decodeError(decoder: ?*pdapi.JSONDecoder, jerror: ?[*:0]const u8, linenum: c_int) callconv(.C) void {
-        const bitlib: *const BitmapLibrary = @ptrCast(@alignCast((decoder orelse return).userdata));
-        bitlib.playdate.system.logToConsole("decodeError: %s %d", jerror, linenum);
+        const jstate: *const BitmapLibParser = @ptrCast(@alignCast((decoder orelse return).userdata));
+        const bitlib = jstate.bitlib;
+        const pd = bitlib.playdate;
+        pd.system.logToConsole("decodeError: %s %d", jerror, linenum);
     }
+
     fn willDecodeSublist(decoder: ?*pdapi.JSONDecoder, name: ?[*:0]const u8, jtype: pdapi.JSONValueType) callconv(.C) void {
-        const bitlib: *BitmapLibrary = @ptrCast(@alignCast((decoder orelse return).userdata));
-        if (jtype == .JSONArray and std.mem.eql(u8, "res", std.mem.sliceTo(name.?, 0))) {
-            bitlib.tmp_is_res = true;
+        const jstate: *BitmapLibParser = @ptrCast(@alignCast((decoder orelse return).userdata));
+        const bitlib = jstate.bitlib;
+        const pd = bitlib.playdate;
+
+        if (jtype == .JSONArray and std.mem.eql(u8, "spec", std.mem.sliceTo(name.?, 0))) {
+            jstate.in_spec = true;
         } else {
-            bitlib.tmp_is_res = false;
+            jstate.in_spec = false;
         }
-        if (debug) bitlib.playdate.system.logToConsole("[%s] willDecodeSublist: %s", decoder.?.path, name);
+        if (debug) pd.system.logToConsole("[%s] willDecodeSublist: %s", decoder.?.path, name);
     }
-    //fn shouldDecodeTableValueForKey(decoder: ?*pdapi.JSONDecoder, key: ?[*:0]const u8) callconv(.C) c_int {
-    //    const bitlib: *const BitmapLibrary = @ptrCast(@alignCast((decoder orelse return 0).userdata));
-    //    bitlib.playdate.system.logToConsole("shouldDecodeTableValueForKey: %s", key);
-    //    return 1;
-    //}
+
+    //fn shouldDecodeTableValueForKey(decoder: ?*pdapi.JSONDecoder, key: ?[*:0]const u8) callconv(.C) c_int {}
+
     fn didDecodeTableValue(decoder: ?*pdapi.JSONDecoder, key: ?[*:0]const u8, value: pdapi.JSONValue) callconv(.C) void {
-        const bitlib: *BitmapLibrary = @ptrCast(@alignCast((decoder orelse return).userdata));
+        const jstate: *BitmapLibParser = @ptrCast(@alignCast((decoder orelse return).userdata));
+        const bitlib = jstate.bitlib;
+        const pd = bitlib.playdate;
 
         if (value.type != @intFromEnum(pdapi.JSONValueType.JSONString)) {
             return;
         }
         const key_name = std.mem.sliceTo(key orelse return, 0);
-        const img_str = std.mem.sliceTo(value.data.stringval, 0);
 
         var row_bytes: c_int = 0;
         var image_width: c_int = 0;
@@ -81,12 +100,12 @@ const BitmapLibrary = struct {
         var mask: [*c]u8 = null;
         var data: [*c]u8 = null;
         if (std.mem.eql(u8, "img", key_name)) {
-            const bitmap = bitlib.playdate.graphics.newBitmap(
-                bitlib.tmp_res[0],
-                bitlib.tmp_res[1],
-                @intFromEnum(pdapi.LCDSolidColor.ColorClear),
+            const bitmap = pd.graphics.newBitmap(
+                jstate.resx,
+                jstate.resy,
+                if (jstate.has_mask) @intFromEnum(pdapi.LCDSolidColor.ColorClear) else @intFromEnum(pdapi.LCDSolidColor.ColorBlack),
             ) orelse return;
-            bitlib.playdate.graphics.getBitmapData(
+            pd.graphics.getBitmapData(
                 bitmap,
                 &image_width,
                 &image_height,
@@ -94,25 +113,26 @@ const BitmapLibrary = struct {
                 &mask,
                 &data,
             );
-            std.debug.assert(bitlib.tmp_res[0] * bitlib.tmp_res[1] == image_width * image_height);
+            std.debug.assert(jstate.resx == image_width and jstate.resy == image_height);
+            const img_str = std.mem.sliceTo(value.data.stringval, 0);
             const decode_size = std.base64.url_safe.Decoder.calcSizeForSlice(img_str) catch {
-                _ = bitlib.playdate.system.realloc(bitmap, 0);
-                bitlib.playdate.system.logToConsole("Failed to calc size of %s", key);
+                _ = pd.system.realloc(bitmap, 0);
+                pd.system.logToConsole("Failed to calc size of %s", key);
                 return;
             };
             if (decode_size == row_bytes * image_height) {
                 std.base64.url_safe.Decoder.decode(data[0..@intCast(row_bytes * image_height)], img_str) catch {
-                    _ = bitlib.playdate.system.realloc(bitmap, 0);
-                    bitlib.playdate.system.logToConsole("Failed to decode %s", key);
+                    _ = pd.system.realloc(bitmap, 0);
+                    pd.system.logToConsole("Failed to decode %s", key);
                     return;
                 };
                 bitlib.addMap(bitmap) catch {
-                    _ = bitlib.playdate.system.realloc(bitmap, 0);
-                    bitlib.playdate.system.logToConsole("Bitmap Library Full");
+                    _ = pd.system.realloc(bitmap, 0);
+                    pd.system.logToConsole("Bitmap Library Full");
                 };
             }
         } else if (std.mem.eql(u8, "img_mask", key_name)) {
-            bitlib.playdate.graphics.getBitmapData(
+            pd.graphics.getBitmapData(
                 bitlib.bitmaps[bitlib.bitmaps.len - 1],
                 &image_width,
                 &image_height,
@@ -120,44 +140,57 @@ const BitmapLibrary = struct {
                 &mask,
                 &data,
             );
+            pd.system.logToConsole("hello");
+            // Instead of checking the value.type for a JSONNull, we rely on the fact that
+            // the spec specified a mask or not for when the LCDBitmap was created.
             if (mask == null) {
-                bitlib.playdate.system.logToConsole("No mask set");
+                pd.system.logToConsole("No mask set");
                 return;
             }
+
+            const img_str = std.mem.sliceTo(value.data.stringval, 0);
             const decode_size = std.base64.url_safe.Decoder.calcSizeForSlice(img_str) catch {
-                bitlib.playdate.system.logToConsole("Failed to calc size of %s", key);
+                pd.system.logToConsole("Failed to calc size of %s", key);
                 return;
             };
             if (decode_size == row_bytes * image_height) {
                 std.base64.url_safe.Decoder.decode(mask[0..@intCast(row_bytes * image_height)], img_str) catch {
-                    bitlib.playdate.system.logToConsole("Failed to decode %s", key);
+                    pd.system.logToConsole("Failed to decode %s", key);
                     return;
                 };
             }
         }
-        if (debug) bitlib.playdate.system.logToConsole("[%s] didDecodeTableValue: %s", decoder.?.path, key);
+        if (debug) pd.system.logToConsole("[%s] didDecodeTableValue: %s", decoder.?.path, key);
     }
-    //fn shouldDecodeArrayValueAtIndex(decoder: ?*pdapi.JSONDecoder, pos: c_int) callconv(.C) c_int {
-    //    const bitlib: *const BitmapLibrary = @ptrCast(@alignCast((decoder orelse return 0).userdata));
-    //    bitlib.playdate.system.logToConsole("shouldDecodeArrayValueAtIndex: %d", pos);
-    //    return 1;
-    //}
+
+    //fn shouldDecodeArrayValueAtIndex(decoder: ?*pdapi.JSONDecoder, pos: c_int) callconv(.C) c_int {}
+
     fn didDecodeArrayValue(decoder: ?*pdapi.JSONDecoder, pos: c_int, value: pdapi.JSONValue) callconv(.C) void {
-        const bitlib: *BitmapLibrary = @ptrCast(@alignCast((decoder orelse return).userdata));
-        if (bitlib.tmp_is_res and pos > 0 and pos < 3 and value.type == @intFromEnum(pdapi.JSONValueType.JSONInteger)) {
-            bitlib.tmp_res[@intCast(pos - 1)] = value.data.intval;
+        const jstate: *BitmapLibParser = @ptrCast(@alignCast((decoder orelse return).userdata));
+        const bitlib = jstate.bitlib;
+        const pd = bitlib.playdate;
+        if (jstate.in_spec and (value.type >= @intFromEnum(pdapi.JSONValueType.JSONTrue) or value.type <= @intFromEnum(pdapi.JSONValueType.JSONInteger))) {
+            switch (pos) {
+                1 => jstate.resx = value.data.intval,
+                2 => jstate.resy = value.data.intval,
+                3 => jstate.has_mask = (value.type == @intFromEnum(pdapi.JSONValueType.JSONTrue)),
+                else => return,
+            }
         }
-        if (debug) bitlib.playdate.system.logToConsole("didDecodeArrayValue: %d", pos);
+        if (debug) pd.system.logToConsole("didDecodeArrayValue: %d", pos);
     }
+
     fn didDecodeSublist(decoder: ?*pdapi.JSONDecoder, name: ?[*:0]const u8, jtype: pdapi.JSONValueType) callconv(.C) ?*anyopaque {
         _ = jtype;
-        const bitlib: *BitmapLibrary = @ptrCast(@alignCast((decoder orelse return null).userdata));
-        bitlib.tmp_is_res = false;
-        if (debug) bitlib.playdate.system.logToConsole("didDecodeSublist: %s", name);
+        const jstate: *BitmapLibParser = @ptrCast(@alignCast((decoder orelse return null).userdata));
+        const bitlib = jstate.bitlib;
+        const pd = bitlib.playdate;
+        jstate.in_spec = false;
+        if (debug) pd.system.logToConsole("didDecodeSublist: %s", name);
         return null;
     }
 
-    fn buildLibrary(self: *BitmapLibrary) void {
+    fn buildLibrary(self: *BitmapLibParser) void {
         var json_decoder = pdapi.JSONDecoder{
             .decodeError = decodeError,
             .willDecodeSublist = willDecodeSublist,
@@ -170,8 +203,8 @@ const BitmapLibrary = struct {
             .returnString = 0,
             .path = null,
         };
-        _ = self.playdate.json.decodeString(&json_decoder, level, null);
-        if (debug) self.playdate.system.logToConsole("[%d] [%d]", self.tmp_res[0], self.tmp_res[1]);
+        _ = self.bitlib.playdate.json.decodeString(&json_decoder, level, null);
+        if (debug) self.bitlib.playdate.system.logToConsole("[%d] [%d]", self.resx, self.resy, self.has_mask);
     }
 };
 
@@ -236,22 +269,11 @@ pub export fn eventHandler(playdate: *pdapi.PlaydateAPI, event: pdapi.PDSystemEv
             //      just crash with no message.
             panic_handler.init(playdate);
 
-            const tree_map = playdate.graphics.loadBitmap("assets/images/test_tree", null).?;
-            var image_width: c_int = 0;
-            var image_height: c_int = 0;
-            var mask: [*c]u8 = undefined;
-            playdate.graphics.getBitmapData(
-                tree_map,
-                &image_width,
-                &image_height,
-                null,
-                &mask,
-                null,
-            );
             if (debug) playdate.system.logToConsole("Json Size: %d\n", level.len);
-            
-            var bitmap_lib = BitmapLibrary.init(playdate);
-            bitmap_lib.buildLibrary();
+
+            var bitmap_lib = BitmapLib.init(playdate);
+            var json_builder = BitmapLibParser{ .bitlib = &bitmap_lib };
+            json_builder.buildLibrary();
 
             const tiled_bg: *TiledBG = @ptrCast(@alignCast(
                 playdate.system.realloc(null, @sizeOf(TiledBG)),
@@ -270,7 +292,6 @@ pub export fn eventHandler(playdate: *pdapi.PlaydateAPI, event: pdapi.PDSystemEv
                     playdate.system.realloc(null, @sizeOf(GlobalState)),
                 ));
             global_state.* = .{
-                .tree = tree_map,
                 .tile_map = tiled_bg,
                 .playdate = playdate,
                 .pos = .{ .x = 0, .y = 0 },
@@ -318,7 +339,6 @@ fn update_and_render(userdata: ?*anyopaque) callconv(.C) c_int {
 
     playdate.graphics.drawBitmap(
         global_state.bitlib.bitmaps[0],
-        //global_state.tree,
         @as(c_int, global_state.pos.x) + 128,
         @as(c_int, global_state.pos.y) + 128,
         .BitmapFlippedY,
